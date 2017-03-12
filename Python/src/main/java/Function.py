a3d6d215fe8a6e5e79f2bd1ecebadb1299ce4c52
fslic <fslic@DESKTOP-MCATCHJ>
2017-03-12 11:53:04 -0700

    function demos

--- Python/src/main/java/Function.py
+++ Python/src/main/java/Function.py
@@ -7,3 +7,20 @@ print(z)
 # The syntax in python is loose, you can omit the arguments when calling a function.
 z = function()
 print(z)
+# If you are providing a part of the arguments, don't forget to give the argument names. 
+z = function(y = 5)
+print(z)
+# A function can take various arguments.
+def function(*xs):
+    sum = 0
+    for x in xs:
+        sum += x
+    return sum
+sum = function(3, 4, 5, 6, 7)
+print(sum)
+# Variables Unpacking
+def function(x, y, z):
+    return x + y + z
+array = [1, 2, 3]
+print(function(*array))
+
